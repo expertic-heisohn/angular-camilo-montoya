e604e3809f30ae0da5cd4ed8ca075ee4bda803c2
camilo <camilomontoya2@gmail.com>
2019-11-20 17:08:18 -0500

    Ejercicio 8 - ventana modal

--- agenda-app/start-here/src/app/app.component.ts
+++ agenda-app/start-here/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
+import { ModalDirective } from "angular-bootstrap-md";
 
 @Component({
   selector: "app-root",
@@ -6,6 +7,7 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
+  @ViewChild(ModalDirective) modal: ModalDirective;
   events: Array<any> = [
     {
       time: "08:00",
